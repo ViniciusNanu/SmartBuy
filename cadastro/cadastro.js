91349333f5b3adffd112ab2cdc278706c325156f
Michele-Costaa <michemichele99@gmail.com>
2024-12-19 11:03:54 -0300

    atualizando javascript

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,3 +1,5 @@
+
+
 var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signup')
 var btnColor = document.querySelector('.btnColor')
@@ -13,3 +15,32 @@ document.querySelector('#btnSignup').addEventListener('click', () => {
     formSignup.style.left = "25px"
     btnColor.style.left = "110px"
 })
+
+function validaLogin(emailIn, passwordIn) {
+    const validaEmail = "usuario123@gmail.com";
+    const validaPassword = "123456";
+
+    if (emailIn === validaEmail && passwordIn === validaPassword) {
+        return true; //login válido
+    } else {
+        return false; //login inválido
+    }
+}
+
+document.getElementById("signin").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const emailIn = document.getElementById("emailIn").value;
+    const passwordIn = document.getElementById("passwordIn").value;
+
+    const isValid = validaLogin(emailIn, passwordIn);
+
+    const mensagemElemento =document.getElementById("mensagem");
+    if (isValid) {
+        mensagemElemento.textContent = "Login realizado com sucesso!";
+        mensagemElemento.style.color = "green";
+    } else {
+        mensagemElemento.textContent = "Email ou senha inválidos!";
+        mensagemElemento.style.color = "red";
+    }
+});
